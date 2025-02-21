802a74e79068d06d9a4cadbad20242cf46e00aa8
EmilyRosas <2233073411@correo.ler.uam.mx>
2025-02-21 08:21:31 -0600

    tarea

---
+++ variables/script.js
@@ -0,0 +1,12 @@
+//variales
+
+let ingrediente = "vainilla"
+let ingrediente1="queso"
+let precio = 20;
+let precio1= 40;
+
+
+//document.write(ingrediente1);
+document.getElementById('demo').innerHTML="<b>EL ingrediente es: <b> " + ingrediente;
+document.getElementById('demo1').innerHTML="<b>El precio de la vainilla es: <b> " + precio;
+document.getElementById('demo2').innerHTML="El precio del queso "+ "es: " + precio1;
