76d49a9d174a82fd86bbad917f4b8bc77a4b4fe8
EmilyRosas <2233073411@correo.ler.uam.mx>
2025-03-07 23:36:11 -0600

    Tarea 2

--- Tarea2/script.js
+++ Tarea2/script.js
@@ -1,16 +1,11 @@
-let p1 ="párrafo 1";
+let p1 ="Las tecnologías html, java y css, se han convertido en herramientas fundamentales para el internet de hoy en día, las aplicaciones son variadas como sus funciones, estás pueden ser para gobiernos, ventas oline, marketing, redes sociales, enseñanza o entretenimiento. Cada una con un estilo que las hace diferentes pero con la misma base, html, java y css. Así que a mi parecer es muy preciso la enseñanza de estas herramientas en la clase y en demás espacios de enseñanza. El enfoque de cada página será diferente, esto por las intenciones, gustos o metas del estudiante.";
 
-let p2 ="párrafo 2";
+let p2 ="La aplicación de estas herramientas no solo se enfoca en lo anterior, hay una salida más, la aplicación hacia el arte. Un arte con diferentes intenciones y formas, como el documental interactivo, arte-archivo, novelas interactivas, juegos en web, narrativas audiovisuales, videos 360 o realidad virtual. Por mi parte quisiera lograr narrativas audiovisuales, y por defecto que sean interactivas. Las historias a contar sean un reflejo de nuestras realidades. Además estaría dispuesta a la creación de juegos en la web, sobre todo por el fácil acceso a estos. ";
 
-let p3 ="párrafo 3";
 
-let p4 ="párrafo 4";
 
 
 document.getElementById('pr1').innerHTML=p1;
 
 document.getElementById('pr2').innerHTML=p2;
 
-document.getElementById('pr3').innerHTML=p3;
-
-document.getElementById('pr4').innerHTML=p4;
