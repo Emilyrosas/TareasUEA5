a4f463bfa35c0457f871f7444522213115c1de7d
EmilyRosas <2233073411@correo.ler.uam.mx>
2025-03-18 10:07:52 -0600

    tarea

---
+++ cadenas/script.js
@@ -0,0 +1,8 @@
+let variable = "Aquí va un texto para el ejercicio";
+
+document.getElementById('cadena').innerHTML= variable; 
+
+console.log("Hola mujer");
+
+//console.log(variable.length);
+console.log(variable.at(17));
